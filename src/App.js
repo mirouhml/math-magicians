cd65f30e9f4392f10e63bb3e6c662a73f165ad3b
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-14 00:39:03 +0100

    Change title of the website.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    const prevTitle = document.title;
+    document.title = 'Math Magicians';
+  
+    return () => {
+      document.title = prevTitle;
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Math Magicians
+        </h1>
       </header>
     </div>
   );
