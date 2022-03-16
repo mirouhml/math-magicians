6b77a16269b39e83e6ae69c2facd3dde61d0779a
mirouhml <amarhamlaoui@gmail.com>
2022-03-16 11:19:21 +0100

    Create a function setupCalculator.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-useless-constructor */
 /* eslint-disable max-len */
 
 import React from 'react';
@@ -7,22 +5,26 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  render() {
-    const buttons = [
+    this.buttons = [
       ['AC', '+/-', '%', '÷'],
       ['7', '8', '9', '×'],
       ['4', '5', '6', '-'],
       ['1', '2', '3', '+'],
     ];
+  }
+
+  setupCalculator() {
+    return this.buttons.map((button) => <tr key={button}>{button.map((cell) => { if (cell === '÷' || cell === '×' || cell === '-' || cell === '+') return <td key={cell} className="operator">{cell}</td>; return <td key={cell}>{cell}</td>; })}</tr>);
+  }
+
+  render() {
     return (
       <table>
         <tr>
           <td className="result" colSpan="4">0</td>
         </tr>
         {
-          buttons.map((button) => <tr key={button}>{button.map((cell) => { if (cell === '÷' || cell === '×' || cell === '-' || cell === '+') return <td key={cell} className="operator">{cell}</td>; return <td key={cell}>{cell}</td>; })}</tr>)
+          this.setupCalculator()
         }
         <tr>
           <td colSpan="2">0</td>
