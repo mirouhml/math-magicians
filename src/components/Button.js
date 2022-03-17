a7d8ec30db50df85ffd011b1ba8eedfa002004aa
mirouhml <amarhamlaoui@gmail.com>
2022-03-17 14:48:16 +0100

    Refactor Button class to extend PureComponent.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,18 @@
+/* eslint-disable react/prop-types */
+
 import React from 'react';
 
-export default class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    Object.assign(this, this.props);
-  }
-
+export default class Button extends React.PureComponent {
   render = () => {
-    const row = this.buttons.map((cell, index) => {
-      if (this.last) {
-        if (index === 0) return <td key={cell} colSpan="2"><button type="button" onClick={() => this.action(cell)}>{cell}</button></td>;
-        if (index === 2) return <td key={cell}><button className="operator" type="button" onClick={() => this.action(cell)}>{cell}</button></td>;
-        return <td key={cell}><button type="button" onClick={() => this.action(cell)}>{cell}</button></td>;
+    const { buttons, action, last } = this.props;
+    const row = buttons.map((cell, index) => {
+      if (last) {
+        if (index === 0) return <td key={cell} colSpan="2"><button type="button" onClick={() => action(cell)}>{cell}</button></td>;
+        if (index === 2) return <td key={cell}><button className="operator" type="button" onClick={() => action(cell)}>{cell}</button></td>;
+        return <td key={cell}><button type="button" onClick={() => action(cell)}>{cell}</button></td>;
       }
-      if (index === 3) return <td key={cell}><button className="operator" type="button" onClick={() => this.action(cell)}>{cell}</button></td>;
-      return <td key={cell}><button type="button" onClick={() => this.action(cell)}>{cell}</button></td>;
+      if (index === 3) return <td key={cell}><button className="operator" type="button" onClick={() => action(cell)}>{cell}</button></td>;
+      return <td key={cell}><button type="button" onClick={() => action(cell)}>{cell}</button></td>;
     });
     return <tr>{row}</tr>;
   };
