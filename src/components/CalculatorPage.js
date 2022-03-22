8d345b261c13ccf1d925f04a18f050d8ba52c73f
mirouhml <amarhamlaoui@gmail.com>
2022-03-22 19:21:42 +0100

    Add calculator page content and styling.

--- src/components/CalculatorPage.js
+++ src/components/CalculatorPage.js
@@ -1,5 +1,14 @@
 import Calculator from './Calculator';
 
-const CalculatorPage = () => <div><Calculator /></div>;
+const CalculatorPage = () => (
+  <div className="background calculator-page">
+    <div>
+      <h2>Let&apos;s do some math!</h2>
+    </div>
+    <div className="calculator">
+      <Calculator />
+    </div>
+  </div>
+);
 
 export default CalculatorPage;
