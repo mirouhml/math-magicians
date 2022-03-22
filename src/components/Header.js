662c756d20305e66e26b95f5a84f5b84320205e0
mirouhml <amarhamlaoui@gmail.com>
2022-03-22 20:02:02 +0100

    Detect current page and hightlight menu item.

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,20 +9,30 @@ import Home from './HomePage';
 import QuotesPage from './QuotesPage';
 import CalculatorPage from './CalculatorPage';
 
+const toggleBackground = (e) => {
+  e.target.parentNode.parentNode.childNodes.forEach((child) => {
+    if (child.classList.contains('active-background')) {
+      const child2 = child;
+      child2.className = '';
+    }
+  });
+  e.target.parentNode.className = 'active-background';
+};
+
 const Header = () => (
   <Router>
     <div className="main">
       <div className="header">
         <h1>Math Magicians</h1>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
+          <li className="active-background">
+            <Link to="/" id="home" onClick={toggleBackground}>Home</Link>
           </li>
           <li>
-            <Link to="/calculator">Calculator</Link>
+            <Link to="/calculator" id="calculator" onClick={toggleBackground}>Calculator</Link>
           </li>
           <li>
-            <Link to="/quotes">Quotes</Link>
+            <Link to="/quotes" id="quotes" onClick={toggleBackground}>Quotes</Link>
           </li>
         </ul>
       </div>
