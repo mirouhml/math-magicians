9d7b5f0593f793a38462bce414b5de3446db93bd
mirouhml <amarhamlaoui@gmail.com>
2022-03-22 19:40:04 +0100

    Add gif in the calculator page.

--- src/components/CalculatorPage.js
+++ src/components/CalculatorPage.js
@@ -4,6 +4,7 @@ const CalculatorPage = () => (
   <div className="background calculator-page">
     <div>
       <h2>Let&apos;s do some math!</h2>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Math_-_Idil_Keysan_-_Wikimedia_Giphy_stickers_2019.gif" alt="Math Gif" />
     </div>
     <div className="calculator">
       <Calculator />
