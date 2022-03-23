9d79555ada11cd5c15f74a3fdd1eb752007b5ec8
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-23 10:40:16 +0200

    Test subtraction  function

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -4,4 +4,8 @@ describe('Unit tests for the operate.js logic script', () => {
   test('Adding two numbers: operate(3, 4, '+')', () => {
     expect(operate(3, 4, '+')).toEqual('7');
   });
+
+  test('Subtracting one number from another: operate(8, 2, '-')', () => {
+    expect(operate(8, 2, '-')).toEqual('6');
+  });
 });
